25cd45ef845a9782a208cf9d8e76bb606ac25cb5
Akremjs <akremkhelifi07@gmail.com>
2026-05-31 12:17:22 +0200

    feat: add out-of-stock badge for Odoo 17 eCommerce

---
+++ out_of_stock_badge/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Badge Rupture de Stock',
+    'version': '17.0.1.0.0',
+    'summary': 'Affiche un badge rouge automatique sur les produits en rupture de stock',
+    'depends': ['website_sale'],
+    'data': ['views/badge.xml'],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
