3d696413527eeba72960067dc235a138df667bb0
Akremjs <akremkhelifi07@gmail.com>
2026-05-31 20:19:17 +0200

    feat: rename module to Smart Out-of-Stock Badge for eCommerce

--- out_of_stock_badge/__manifest__.py
+++ out_of_stock_badge/__manifest__.py
@@ -4,7 +4,6 @@
     'summary': 'Automatic out-of-stock badge on product images when qty = 0',
     'category': 'Website/Website',
     'author': 'AKREM.KHELIFI',
-    'website': 'https://github.com/Akremjs/Auto-Out-of-Stock-Ribbon-for-eCommerce',
     'license': 'LGPL-3',
     'depends': ['website_sale'],
     'data': ['views/badge.xml'],
